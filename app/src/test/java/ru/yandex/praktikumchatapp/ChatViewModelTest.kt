c1cc339cd83e73ac3a2599f00e1e9e9d4e0cce9f
Anar <dtalievanar@gmail.com>
2025-04-01 16:04:16 +0300

    добавлен юнит тест для вьюмодели

--- app/src/test/java/ru/yandex/praktikumchatapp/ChatViewModelTest.kt
+++ app/src/test/java/ru/yandex/praktikumchatapp/ChatViewModelTest.kt
@@ -1,7 +1,10 @@
+import app.cash.turbine.test
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
@@ -10,6 +13,7 @@ import org.junit.Before
 import org.junit.Test
 import ru.yandex.praktikumchatapp.presentation.ChatViewModel
 import ru.yandex.praktikumchatapp.presentation.Message
+import kotlin.test.assertEquals
 
 @ExperimentalCoroutinesApi
 class ChatViewModelTest {
@@ -31,13 +35,37 @@ class ChatViewModelTest {
 
     @Test
     fun `send message should update messages with MyMessage`() = runTest {
-        val message = Message.MyMessage("TestMessage")
+        val testMessage = "TestMessage"
 
+        viewModel.messages.test {
+            assertEquals(emptyList(), awaitItem())
+            viewModel.sendMyMessage(testMessage)
+            val updatedMessages = awaitItem()
+            assertEquals(1, updatedMessages.size)
+            val message = updatedMessages.first()
+            assertEquals(Message.MyMessage(testMessage), message)
+        }
     }
+
 
     @Test
     fun testReceiveMessage_concurrentMessages() = runTest {
         val messagesToSend = (1..100).map { Message.MyMessage("Message $it") }
-
+        val collectJob = launch {
+            viewModel.messages.test {
+                awaitItem()
+                repeat(100) {
+                    awaitItem()
+                }
+            }
+        }
+        messagesToSend.forEach { message ->
+            launch {
+                viewModel.sendMyMessage(message.text)
+            }
+        }
+        advanceUntilIdle()
+        assertEquals(100, viewModel.messages.value.size)
+        collectJob.cancel()
     }
 }
